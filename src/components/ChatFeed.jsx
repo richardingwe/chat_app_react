13f1e28850d9f04f1911edd359c30fc88010eff2
richardingwe <richardingwe5@gmail.com>
2021-11-13 12:22:42 +0100

    finished with chat feed

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -7,13 +7,29 @@ const ChatFeed = (props) => {
 
 	const chat = chats && chats[activeChat];
 
+	const renderReadReceipts = (message, isMyMessage) =>
+		chat.people.map(
+			(person, index) =>
+				person.last_read === message.id && (
+					<div
+						key={`read_${index}`}
+						className='read-receipt'
+						style={{
+							float: isMyMessage ? 'right' : 'left',
+							backgroundImage:
+								person.person.avatar && `url(${person.person.avatar})`,
+						}}
+					/>
+				)
+		);
+
 	const renderMessages = () => {
 		const keys = Object.keys(messages);
 
 		return keys.map((key, index) => {
 			const message = messages[key];
 			const lastMessageKey = index === 0 ? null : keys[index - 1];
-			const isMyMessage = userName === message.sender.userName;
+			const isMyMessage = userName === message.sender.username;
 
 			return (
 				<div key={`msg_${index}`} style={{ width: '100%' }}>
@@ -33,14 +49,12 @@ const ChatFeed = (props) => {
 							marginRight: isMyMessage ? '18px' : '0px',
 							marginLeft: isMyMessage ? '0px' : '68px',
 						}}>
-						read-receipts
+						{renderReadReceipts(message, isMyMessage)}
 					</div>
 				</div>
 			);
 		});
 	};
-
-	renderMessages();
 
 	if (!chat) return <div />;
 
