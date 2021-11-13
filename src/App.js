4f88fcfa7b63ff0d402816838bd7a8dc27db4101
richardingwe <richardingwe5@gmail.com>
2021-11-13 11:51:18 +0100

    created a chat feed component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
 import './App.css';
-
 
 const App = () => {
 
@@ -10,6 +10,7 @@ const App = () => {
             projectID='a60477e4-d163-4af5-9de7-2bc9fe2cc6d2'
             userName='rui'
             userSecret='12345'
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
         />
     );
 };
